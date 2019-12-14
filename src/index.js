32d52c60ab764d668f9114c706e4b263952e235b
Kyle <wilichowskikyle@gmail.com>
2019-12-14 00:55:08 -0600

    fix missing import err

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import Counters from "./components/counters";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(<Counters />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 serviceWorker.unregister();
