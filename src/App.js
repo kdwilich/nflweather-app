227d8f5d5510ce5e611f4156c5d9f10f1187f83e
Kyle <wilichowskikyle@gmail.com>
2019-12-15 18:55:38 -0600

    pull in data from nfl feed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,68 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Weather App</p>
-      </header>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      schedules: []
+    };
+  }
+
+  componentWillUnmount() {
+    document.body.style.backgroundColor = null;
+    document.body.style.color = null;
+  }
+
+  componentDidMount() {
+    document.body.style.backgroundColor = "#282c34";
+    document.body.style.color = "white";
+    this.getSchedules();
+  }
+
+  getSchedules = _ => {
+    fetch("https://feeds.nfl.com/feeds-rs/schedules.json")
+      .then(response => response.json())
+      .then(response => this.setState({ schedules: response.gameSchedules }))
+      .catch(err => console.error(err));
+  };
+
+  renderSchedules = ({
+    gameDate,
+    seasonType,
+    gameId,
+    gameTimeLocal,
+    homeTeamAbbr,
+    visitorTeamAbbr,
+    week
+  }) => (
+    <div key={gameId}>
+      {seasonType === "REG" && week === 15 && (
+        <div>
+          {homeTeamAbbr} vs {visitorTeamAbbr} on {gameDate} at &nbsp;
+          {this.formatTime(gameTimeLocal)}
+          <br />
+          &nbsp;
+        </div>
+      )}
     </div>
   );
+
+  formatTime(time) {
+    time = time.split(":");
+    let hour = parseInt(time[0].substring(0, 2), 10);
+    let timeOfDay;
+    hour < 12 ? (timeOfDay = "AM") : (timeOfDay = "PM");
+    hour = ((hour + 11) % 12) + 1;
+
+    return `${hour.toString()}:${time[1]} ${timeOfDay}`;
+  }
+
+  render() {
+    const { schedules } = this.state;
+    // /schedules.map(this.renderSchedules)
+    return <div>{schedules.map(this.renderSchedules)}</div>;
+  }
 }
 
 export default App;
