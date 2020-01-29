70943e17a7d4d012acf2e530c87b07d85f83ad41
Kyle Wilichowski <kdwilich@uark.edu>
2020-01-29 16:26:49 -0600

    compile json list of cities/states with respective lon/lat

--- src/assets/CityCoords.js
+++ src/assets/CityCoords.js
@@ -3,227 +3,221 @@ const SITES = {
     {
       city: "Canton",
       state: "OH",
-      longitude: "",
-      latitude: ""
+      longitude: "-81.377037",
+      latitude: "40.797390"
     },
     {
       city: "Orchard Park",
       state: "NY",
-      longitude: "",
-      latitude: ""
+      longitude: "-78.743912",
+      latitude: "42.767460"
     },
     {
       city: "East Rutherford",
       state: "NJ",
-      longitude: "",
-      latitude: ""
+      longitude: "-74.097893",
+      latitude: "40.831799"
     },
     {
       city: "Cleveland",
       state: "OH",
-      longitude: "",
-      latitude: ""
+      longitude: "-81.6934446",
+      latitude: "41.5051613"
     },
     {
       city: "Miami Gardens",
       state: "FL",
-      longitude: "",
-      latitude: ""
+      longitude: "-80.2397649",
+      latitude: "25.9423108"
     },
     {
       city: "Baltimore",
       state: "MD",
-      longitude: "",
-      latitude: ""
+      longitude: "-76.610759",
+      latitude: "39.2908816"
     },
     {
       city: "Philadelphia",
       state: "PA",
-      longitude: "",
-      latitude: ""
+      longitude: "-75.1635262",
+      latitude: "39.9527237"
     },
     {
       city: "Detroit",
       state: "MI",
-      longitude: "",
-      latitude: ""
+      longitude: "-83.0466403",
+      latitude: "42.3315509"
     },
     {
       city: "Chicago",
       state: "IL",
-      longitude: "",
-      latitude: ""
+      longitude: "-87.6244212",
+      latitude: "41.8755616"
     },
     {
       city: "Green Bay",
       state: "WI",
-      longitude: "",
-      latitude: ""
+      longitude: "-88.0125794",
+      latitude: "44.5126379"
     },
     {
       city: "Seattle",
       state: "WA",
-      longitude: "",
-      latitude: ""
+      longitude: "-122.3300624",
+      latitude: "47.6038321"
     },
     {
       city: "Glendale",
       state: "AZ",
-      longitude: "",
-      latitude: ""
+      longitude: "-112.1859941",
+      latitude: "33.5386858"
     },
     {
       city: "Pittsburgh",
       state: "PA",
-      longitude: "",
-      latitude: ""
+      longitude: "-79.9900861",
+      latitude: "40.4416941"
     },
     {
       city: "New Orleans",
       state: "LA",
-      longitude: "",
-      latitude: ""
+      longitude: "-90.0701156",
+      latitude: "29.9499323"
     },
     {
       city: "Oakland",
       state: "CA",
-      longitude: "",
-      latitude: ""
+      longitude: "-64.3551639",
+      latitude: "44.4521637"
     },
     {
       city: "Kansas City",
       state: "MO",
-      longitude: "",
-      latitude: ""
+      longitude: "-94.5781416",
+      latitude: "39.100105"
     },
     {
       city: "Santa Clara",
       state: "CA",
-      longitude: "",
-      latitude: ""
+      longitude: "-121.6846349",
+      latitude: "37.2333253"
     },
     {
       city: "Jacksonville",
       state: "FL",
-      longitude: "",
-      latitude: ""
+      longitude: "-81.655651",
+      latitude: "30.3321838"
     },
     {
       city: "Landover",
       state: "MD",
-      longitude: "",
-      latitude: ""
+      longitude: "-76.8966397",
+      latitude: "38.9340002"
     },
     {
       city: "Atlanta",
       state: "GA",
-      longitude: "",
-      latitude: ""
+      longitude: "-84.3901849",
+      latitude: "33.7490987"
     },
     {
       city: "Charlotte",
       state: "NC",
-      longitude: "",
-      latitude: ""
+      longitude: "-80.8431268",
+      latitude: "35.2270869"
     },
     {
       city: "Tampa",
       state: "FL",
-      longitude: "",
-      latitude: ""
+      longitude: "-82.458444",
+      latitude: "27.9477595"
     },
     {
       city: "Indianapolis",
       state: "IN",
-      longitude: "",
-      latitude: ""
+      longitude: "-86.1583502",
+      latitude: "39.7683331"
     },
     {
       city: "Nashville",
       state: "TN",
-      longitude: "",
-      latitude: ""
+      longitude: "-86.7743531",
+      latitude: "36.1622296"
     },
     {
       city: "Houston",
       state: "TX",
-      longitude: "",
-      latitude: ""
+      longitude: "-95.3676974",
+      latitude: "29.7589382"
     },
     {
       city: "Honolulu",
       state: "HI",
-      longitude: "",
-      latitude: ""
+      longitude: "-157.8556764",
+      latitude: "21.304547"
     },
     {
       city: "Carson",
       state: "CA",
-      longitude: "",
-      latitude: ""
+      longitude: "-75.6897965",
+      latitude: "45.4191995"
     },
     {
       city: "Minneapolis",
       state: "MN",
-      longitude: "",
-      latitude: ""
+      longitude: "-93.2654692",
+      latitude: "44.9772995"
     },
     {
       city: "Denver",
       state: "CO",
-      longitude: "",
-      latitude: ""
+      longitude: "-104.9848623",
+      latitude: "39.7392364"
     },
     {
       city: "Cincinnati",
       state: "OH",
-      longitude: "",
-      latitude: ""
-    },
-    {
-      city: "Cincinnati",
-      state: "OH",
-      longitude: "",
-      latitude: ""
+      longitude: "-84.5124602",
+      latitude: "39.1014537"
     },
     {
       city: "Foxborough",
       state: "MA",
-      longitude: "",
-      latitude: ""
+      longitude: "-71.2478308",
+      latitude: "42.0653768"
     },
     {
       city: "Winnipeg",
       state: "MB",
-      longitude: "",
-      latitude: ""
+      longitude: "-97.1384584",
+      latitude: "49.8955367"
     },
     {
       city: "Arlington",
       state: "TX",
-      longitude: "",
-      latitude: ""
+      longitude: "-97.105623790",
+      latitude: "32.701938999"
     },
     {
       city: "Los Angeles",
       state: "CA",
-      longitude: "",
-      latitude: ""
+      longitude: "-118.2427666",
+      latitude: "34.0536909"
     },
     {
       city: "Tottenham",
-      longitude: "",
-      latitude: ""
+      longitude: "-0.0672892",
+      latitude: "51.5976955"
     },
     {
       city: "London",
-      longitude: "",
-      latitude: ""
+      longitude: "-0.1276474",
+      latitude: "51.507321"
     },
     {
       city: "Mexico City",
-      longitude: "",
-      latitude: ""
+      longitude: "-99.1333416",
+      latitude: "19.4326009"
     }
   ]
 };
